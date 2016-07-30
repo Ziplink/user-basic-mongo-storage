b616046a03922f4f868d01d89910d0d13a4de2d5
aussieshibe <nnelson263@gmail.com>
2016-07-30 10:19:02 +0000

    Updated tests to reflect new promise based setup

--- test/index.js
+++ test/index.js
@@ -7,7 +7,7 @@ const TEST_DATA = [
     displayName: 'Test Name',
     authentication: {
       provider: 'google',
-      ID: 'testID'
+      id: 'testID'
     }
   }
 ];
@@ -20,16 +20,16 @@ describe('API v0.1', function(){
       
       it('creates User from TEST_DATA[' + index + ']', function(done){
 
-        User.create(testData, function(err, user){
-          
-          expect(user).to.exist;
-          expect(user.name).to.equal(testData.name);
-            
-          if(user)
-            testData.ID = user.ID;
-            
-          done(err);
-        });
+        User.create(testData)
+          .then(function(user) {
+            expect(user).to.exist;
+            expect(user.name).to.equal(testData.name);
+            if(user) {
+              testData.id = user.id;
+            }
+            done();
+          })
+          .catch(done);
         
       });
       
@@ -42,15 +42,14 @@ describe('API v0.1', function(){
     TEST_DATA.forEach(function(testData, index){
       
       it('finds correct User from TEST_DATA[' + index + ']', function(done){
-        User.findByAuthentication(testData.authentication, function(err, user){
-          
-          expect(user).to.exist;
-          expect(user.name).to.equal(testData.name);
-          expect(user.ID).to.equal(testData.ID);
-          
-          done(err);
-          
-        });
+        User.findByAuthentication(testData.authentication)
+          .then(function(user) {
+            expect(user).to.exist;
+            expect(user.name).to.equal(testData.name);
+            expect(user.ID).to.equal(testData.ID);
+            done();
+          })
+          .catch(done);
         
       });
       
